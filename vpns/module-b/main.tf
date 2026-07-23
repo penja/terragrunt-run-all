fa81f7b95aa3d8c1146b4c6c006d07022e1a5c8a
Andrii Penchuk <penja7@gmail.com>
2026-07-22 19:47:47 -0700

    Update SSM parameter value for vpns-b

--- vpns/module-b/main.tf
+++ vpns/module-b/main.tf
@@ -5,6 +5,6 @@ variable "vpc_param_arn" {
 resource "aws_ssm_parameter" "vpns-b" {
   name        = "/cloud-4527-repro/vpns-marker-bс"
   type        = "String"
-  value       = "vpns"
+  value       = "vpns-3"
   description = "depends on ${var.vpc_param_arn}"
 }
