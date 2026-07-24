f4561d119d2f453773ed439c50129c34b0b3a102
Andrii Penchuk <penja7@gmail.com>
2026-07-23 18:33:38 -0700

    Update main.tf

--- vpns/module-a/main.tf
+++ vpns/module-a/main.tf
@@ -2,6 +2,25 @@ variable "vpc_param_arn" {
   type = string
 }
 
+variable "tags" {
+  type = map(string)
+  default = {
+    Environment = "Test"
+    Name        = "Provider Tag"
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = merge(
+      var.tags,
+      {
+        VpcParamArn = var.vpc_param_arn
+      }
+    )
+  }
+}
+
 resource "aws_ssm_parameter" "vpns-a" {
   name        = "/cloud-4527-repro/vpns-marker"
   type        = "String"
