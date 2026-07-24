ec2b481636daae33bfd30adace6cb10266023807
Andrii Penchuk <penja7@gmail.com>
2026-07-23 20:48:03 -0700

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -6,16 +6,7 @@ variable "tags" {
   }
 }
 
-provider "aws" {
-  default_tags {
-    tags = merge(
-      var.tags,
-      {
-        VpcParamArn = "test"
-      }
-    )
-  }
-}
+
 
 resource "aws_ssm_parameter" "vpc" {
   name  = "/cloud-4527-repro/vpc-marker"
